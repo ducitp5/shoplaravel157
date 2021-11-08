930a28ff34e4dda02e618adeb7db202749cb6b53
ASUS-PC-DUC\ADMIN <ductranp2@gmail.com>
2021-11-09 02:48:15 +0700

    $ php artisan migrate --path='./database/migrations/2021_11_08_181028_loyal_customers.php' $ composer dump-autoload $ php artisan db:seed --class=LoyalCustomersTableSeeder

---
+++ database/seeds/Loyal_customers_TableSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class LoyalCustomersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    
+    public function run()
+    {
+        //
+        $data = [
+            [
+                'email' => 'lxc150896@gmail.com',
+                'password' => bcrypt('12345'),
+            ],
+            [
+                'email' => 'lxc@gmail.com',
+                'password' => bcrypt('12345'),
+            ],
+            [
+                'email' => 'admin@gmail.com',
+                'password' => bcrypt('12345'),
+            ],
+        ];
+        DB::table('loyal_customers')->insert($data);
+    }
+}
